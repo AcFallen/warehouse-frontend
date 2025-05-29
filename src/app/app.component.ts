6b33408196c3d617a3291d4fbd1ba9ba9a49250e
Roberto Carlos <r.apaza.cornejo@gmail.com>
2025-05-29 16:41:01 -0500

    Add Angular Material components and update project structure
    - Integrated Angular Material with new dependencies for CDK and Material.
    - Added analytics tracking ID in angular.json.
    - Included Azure Blue theme in styles.
    - Created a login component with a card layout and basic styling.
    - Set up routing to redirect to the login page.
    - Removed placeholder app component HTML and test files.
    - Updated global styles and index.html for typography and font links.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, MatSlideToggleModule],
+  template: ` <router-outlet></router-outlet> `,
 })
 export class AppComponent {
   title = 'warehouse-frontend';
---
+++ src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
