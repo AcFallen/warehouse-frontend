71bdb7fc419bd8778ec49208b8d6bd4b069d0924
Roberto Carlos <r.apaza.cornejo@gmail.com>
2025-05-29 17:42:50 -0500

    Update Angular Material and CDK to version 20.0.1; refactor login component layout and styles
    - Upgraded @angular/cdk and @angular/material dependencies to version 20.0.1 in package.json and pnpm-lock.yaml.
    - Refactored login component to use a new form layout with Material design.
    - Updated global styles to incorporate Angular Material theming.
    - Enhanced login component styles for better user experience.

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
